3634fd7b1f8d333711c1429924d27bc1c8fb328c
Todd Mullen <ToddJ.Mullen@Gmail.com>
2018-03-01 17:52:36 -0500

    Disable experimental routing view. - Genrate package-lock.json

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use \Illuminate\Http\Request;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,24 +17,43 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get("posts", function(){
 
-Route::get("testRoute", function(){
-	return "Test route worked";
+//	if( view()->exists("posts.index") ){
+//		return "The named view exists";
+//	}
+//	else {
+//		return "The named view does not exist.";
+//	}
+
+	$title	= "Another title";
+	$other	= "Bob's Jumps";
+	$var3	= "var3";
+	return view("posts.index", compact("title","other","var3") );
 });
 
-Route::match(["get","post"], "doubleVerbs", function(){
-	return "Get or post works here";
-});
-
-Route::any("anyUrl", function(){
-	return "any verb works here";
-});
-
-Route::get("ageTest",
-	["middleware"=>"ageCheck", function(){
-	return "ageTest() passed";
-}]);
-
-Route::get("home", function(){
-	return "Welcome home!";
-});
+//Route::get("testRoute", function(){
+//	return "Test route worked";
+//});
+//
+//Route::match(["get","post"], "doubleVerbs", function(){
+//	return "Get or post works here";
+//});
+//
+//Route::any("anyUrl", function(){
+//	return "any verb works here";
+//});
+//
+//Route::get("ageTest",
+//	["middleware"=>"ageCheck", function(){
+//	return "ageTest() passed";
+//}]);
+//
+//Route::get("home", function(){
+//	return "Welcome home!!!";
+//});
+//
+//Route::get("request-test", function( Request $request ){
+////	return $request->input("age");
+//	return $request->all();//returns all request params
+//});
