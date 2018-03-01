202d3ee8e24a1cb00812c35a569a56beb32f807a
Todd Mullen <ToddJ.Mullen@Gmail.com>
2018-03-01 17:55:23 -0500

    Create cliTest.php file to experiment with php execution from the command line.

---
+++ cliTest.php
@@ -0,0 +1,13 @@
+<?php
+
+/***********************
+ * Project	: bloggy.loc
+ * File		: cliTest.php
+ * Created	: Mar 1, 2018, 5:18:50 PM
+ * @author	: Todd Mullen
+ * Description : cliTest.php
+ ***********************/
+
+echo "This is my CLI test file\n"
+	. " This was built as an example that I can execute the script from the command line.\n";
+
