8c37cdee3800bd08ff353bc8d450d400e599f194
Todd Mullen <ToddJ.Mullen@Gmail.com>
2018-03-11 20:26:39 -0400

    master.blade.php, change page relative resource (js/css) paths to site relative paths.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -17,9 +17,9 @@
 		<meta charset="UTF-8">
 		<!--		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" type="text/css">
 				<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" rel="stylesheet" type="text/css">-->
-		<link href="css/simple-sidebar.css" rel="stylesheet">
+		<link href="/css/simple-sidebar.css" rel="stylesheet">
 
-		<link href="css/app.css" rel="stylesheet" type="text/css">
+		<link href="/css/app.css" rel="stylesheet" type="text/css">
 
 <!--		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.js" type="text/javascript"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" type="text/javascript"></script>-->
@@ -92,9 +92,9 @@
 		<!-- /#wrapper -->
 
 		<!-- Bootstrap core JavaScript -->
-		<script src="vendor/jquery/jquery.min.js"></script>
-		<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-		<script src="js/app.js" type="text/javascript"></script>
+		<script src="/vendor/jquery/jquery.min.js"></script>
+		<script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+		<script src="/js/app.js" type="text/javascript"></script>
 
 		<!-- Menu Toggle Script -->
 		<script>
