d102823c598cca8c85ea617f7b88af665be14021
Todd Mullen <ToddJ.Mullen@Gmail.com>
2018-03-15 21:28:34 -0400

    posts/index.blade.php, Add Posts ID to Post titles

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,7 +5,10 @@
 @section("content")
 
 	@foreach( $posts as $post )
-	<h4 class="postTitle"><a href="{{ action( "PostsController@show", $post->id ) }}">{{ $post->title }}</a></h4>
+	<h4 class="postTitle">
+		{{ $post->id }})
+		<a href="{{ action( "PostsController@show", $post->id ) }}">{{ $post->title }}</a>
+	</h4>
 	<article class="border-left ">
 		{{ $post->body }}
 	</article>
