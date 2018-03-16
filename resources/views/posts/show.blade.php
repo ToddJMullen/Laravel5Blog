048eb71d5019148fc053c039fc3f2e5d0d082576
Todd Mullen <ToddJ.Mullen@Gmail.com>
2018-03-15 21:30:04 -0400

    posts/show.blade.php, Add missing fields to delete form

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,11 +6,15 @@
 
 	<h4>{{ $post->title }} <a href="/posts" class="pull-right">All Posts</a></h4>
 
-	<article class="border-left ">
+	<article class="border-left p-2">
+
 		{{ $post->body }}
+
 		<form action="{{ action('PostsController@destroy', $post->id) }}"
 			  method="POST" class="text-right" >
+			<input name="_method" value="DELETE" type="hidden">
 			<button class="btn btn-danger pull-right">Delete</button>
+			{{ csrf_field() }}
 		</form>
 	</article>
 
