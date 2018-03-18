b35c83deda4ed7e7a4b97fdd4e6047775be85829
Todd Mullen <ToddJ.Mullen@Gmail.com>
2018-03-18 15:04:22 -0400

    index.blade.php, Add link on index page for editing Posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,6 +8,8 @@
 	<h4 class="postTitle">
 		{{ $post->id }})
 		<a href="{{ action( "PostsController@show", $post->id ) }}">{{ $post->title }}</a>
+		<a href="{{ action( "PostsController@edit", $post->id ) }}"
+		   class="btn btn-sm btn-warning pull-right">Edit</a>
 	</h4>
 	<article class="border-left ">
 		{{ $post->body }}
