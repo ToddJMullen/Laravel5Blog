5dafdcc16ca2ae5dff6c1f6230e33196652104c9
Todd Mullen <ToddJ.Mullen@Gmail.com>
2018-03-18 17:09:58 -0400

    Inventory.php, Add the $fillable / mass assignable property to list fields that can be set in mass assignment queries.

--- app/Inventory.php
+++ app/Inventory.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Inventory extends Model
 {
-    //
+
+	/**
+	 *	List the VO fields that are allowed to be assigned with mass (?unlimited/unspecific?) queries
+	 * @var array $fillable
+	 */
+	protected $fillable = [
+		"name", "description", "qty", "price"
+	];
 }
