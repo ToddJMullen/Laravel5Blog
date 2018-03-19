e2c06553c13e2a847595aa7cdfd61a2df6fb8f2a
Todd Mullen <ToddJ.Mullen@Gmail.com>
2018-03-18 20:34:59 -0400

    ItemTableSeeder, Setup faker column data generator attributes in for loop.

--- database/seeds/ItemTableSeeder.php
+++ database/seeds/ItemTableSeeder.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Database\Seeder;
 
+use Faker\Factory as Faker;
+//use Faker\ORM\Doctrine\Populator;
+use App\Item;
+
 
 class ItemTableSeeder extends Seeder
 {
@@ -12,6 +16,19 @@ class ItemTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+		$gen = Faker::create();
+
+		for( $i = 0; $i < 200; $i++ ){
+
+			Item::create([
+				"user_id"		=> rand(1, 30)
+				,"date_bought"	=> $gen->dateTimeThisYear()
+				,"name"			=> $gen->domainWord()
+				,"qty"			=> rand(1, 500 )
+				,"price"		=> $gen->randomFloat(4, 0, 7)// rand(1,5) * sin(rand(0,6))
+				,"description"	=> $gen->paragraph()//s(2) paragraphs() returns an array
+				,"notes"		=> $gen->sentence()//s(2) sentences() returns an array
+			]);
+		}//for( $i )
     }
 }
