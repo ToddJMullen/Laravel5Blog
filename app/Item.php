67c397117daa661040f7a38e1bcbb93812d891a1
Todd Mullen <ToddJ.Mullen@Gmail.com>
2018-03-18 23:05:31 -0400

    Item, Define mass assignable fields.

--- app/Item.php
+++ app/Item.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Item extends Model
 {
-    //
+
+	/**
+	 *	List the VO fields that are allowed to be assigned with mass (?unlimited/unspecific?) queries
+	 * @var array $fillable
+	 */
+	protected $fillable = [
+		"name", "description", "price", "qty", "description", "notes"
+	];
+	
 }
