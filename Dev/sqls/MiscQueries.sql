054f18354839d83e4c7428762aaf743147065c03
Todd Mullen <ToddJ.Mullen@Gmail.com>
2018-03-18 20:35:48 -0400

    MiscQueries.sql, Add queries for Items & Users tables introspection.

--- Dev/sqls/MiscQueries.sql
+++ Dev/sqls/MiscQueries.sql
@@ -3,7 +3,15 @@ SHOW tables WHERE 1;
 
 SELECT * FROM migrations WHERE 1;
 
+SELECT * FROM users WHERE 1;
+SELECT count(*) FROM users where 1;
+
 SELECT * FROM posts WHERE 1 LIMIT 300;
 
-SELECT * FROM `items` WHERE 1 LIMIT 50;
+SELECT * FROM `items`
+ WHERE 1
+ ORDER BY `id` DESC
+ LIMIT 50;
+
+SELECT COUNT(*) FROM `items` WHERE 1;
 
