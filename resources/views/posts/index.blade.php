2564f1110d51ff7fdacedf4dd97671104716349f
Todd Mullen <ToddJ.Mullen@Gmail.com>
2018-03-22 20:45:23 -0400

    index.blade, Change layout of edit & delete buttons.

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,26 +4,28 @@
 
 @section("content")
 
-	@foreach( $posts as $post )
-	<h4 class="postTitle">
+@foreach( $posts as $post )
+<div class="row">
+	<h4 class="postTitle col-sm-12">
 		{{ $post->id }})
 		<a href="{{ action( "PostsController@show", $post->id ) }}">{{ $post->title }}</a>
-		<a href="{{ action( "PostsController@edit", $post->id ) }}"
-		   class="btn btn-sm btn-warning pull-right">Edit</a>
 	</h4>
-	<article class="border-left ">
+	<article class="border-right col-sm-11">
 		{{ $post->body }}
 	</article>
-	<div class="text-right col-sm-12">
+	<div class="col-sm-1 text-right">
 		<form action="{{ action('PostsController@destroy', $post->id ) }}" method="POST">
 			<input name="_method" value="DELETE" type="hidden">
-			<button class="btn btn-danger pull-right">Delete</button>
+			<button class="btn btn-danger pull-left">Delete</button>
 			{{csrf_field()}}
 		</form>
+		<a href="{{ action( "PostsController@edit", $post->id ) }}"
+		   class="btn btn-warning pullleft- mt-3">Edit</a>
 	</div>
-	<hr />
-	@endforeach
+</div>
+<hr />
+@endforeach
 
-	{{ $posts->links() }}
+{{ $posts->links() }}
 
 @endsection
